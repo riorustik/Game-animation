2c6928509f1ade732dc882734a2302813d5a1217
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-06 21:51:34 +0400

    fix addEventListener

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ for (let i = 0; i < SQUARES_NUMBER; i++){
     square.className = 'square';
     square.addEventListener('mouseover', () => setColor(square));
     square.addEventListener('mouseleave', () => removeColor(square));
-    square.addEventListener('touchstart', () => setColor(square));
-    square.addEventListener('touchend', () => removeColor(square));
 
     board.append(square);
 }
